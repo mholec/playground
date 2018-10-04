920289632cd15fd33fba3c655a155617f7596775
Miroslav Holec <mirek.holec@hotmail.com>
2018-10-04 10:11:59 +0200

    Update prezentace

--- aspnetcore/hostovani-deployment-runtimes/CoreVersions/Startup.cs
+++ aspnetcore/hostovani-deployment-runtimes/CoreVersions/Startup.cs
@@ -13,30 +13,23 @@ namespace CoreVersions
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+	    app.UseDeveloperExceptionPage();
 
-	        var current = Assembly.GetExecutingAssembly();
-	        var whb = Assembly.GetAssembly(typeof(WebHostBuilder));
-	        var all = AppDomain.CurrentDomain.GetAssemblies().Select(x=> x.FullName);
-
+	    var assemblies = AppDomain.CurrentDomain.GetAssemblies().Select(x=> x.FullName);
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync(JsonConvert.SerializeObject(all, new JsonSerializerSettings()
+                await context.Response.WriteAsync(JsonConvert.SerializeObject(assemblies, new JsonSerializerSettings()
                 {
-					ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+		   ReferenceLoopHandling = ReferenceLoopHandling.Ignore
                 }));
             });
         }
