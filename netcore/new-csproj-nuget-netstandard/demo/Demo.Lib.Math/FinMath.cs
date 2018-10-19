2f51a752627c046d9d951d18e2ba6e6ac4777bac
Miroslav Holec <mirek.holec@hotmail.com>
2018-10-19 17:03:34 +0200

    Version 1.0.1 test

---
+++ netcore/new-csproj-nuget-netstandard/demo/Demo.Lib.Math/FinMath.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo.Lib.Math
+{
+    public class FinMath
+    {
+	    public decimal Plus(int num1, int num2)
+	    {
+		    return num1 + num2;
+	    }
+	}
+}
---
+++ netcore/new-csproj-nuget-netstandard/demo/Demo.Lib.Text/Words.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Demo.Lib.Text
+{
+	public static class Words
+	{
+		public static int WordCount(this string str)
+		{
+			return str.Split(new char[] { ' ', '.', '?' },
+				StringSplitOptions.RemoveEmptyEntries).Length;
+		}
+	}
+}
