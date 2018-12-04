a551bf4c66b26a50e606710e108540203f753415
Miroslav Holec <mirek.holec@hotmail.com>
2018-12-04 07:54:08 +0100

    Windows Service example

---
+++ netcore/windows-service/demo/WinService/Infrastructure/ServiceBaseLifetime.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace WinService.Infrastructure
+{
+	public class ServiceBaseLifetime : ServiceBase, IHostLifetime
+	{
+		private readonly TaskCompletionSource<object> delayStart = new TaskCompletionSource<object>();
+
+		public ServiceBaseLifetime(IApplicationLifetime applicationLifetime)
+		{
+			ApplicationLifetime = applicationLifetime ?? throw new ArgumentNullException(nameof(applicationLifetime));
+		}
+
+		private IApplicationLifetime ApplicationLifetime { get; }
+
+		public Task WaitForStartAsync(CancellationToken cancellationToken)
+		{
+			cancellationToken.Register(() => delayStart.TrySetCanceled());
+			ApplicationLifetime.ApplicationStopping.Register(Stop);
+
+			new Thread(Run).Start();
+			return delayStart.Task;
+		}
+
+		private void Run()
+		{
+			try
+			{
+				Run(this);
+				delayStart.TrySetException(new InvalidOperationException("Stopped without starting"));
+			}
+			catch (Exception ex)
+			{
+				delayStart.TrySetException(ex);
+			}
+		}
+
+		public Task StopAsync(CancellationToken cancellationToken)
+		{
+			Stop();
+			return Task.CompletedTask;
+		}
+
+		protected override void OnStart(string[] args)
+		{
+			delayStart.TrySetResult(null);
+			base.OnStart(args);
+		}
+
+		protected override void OnStop()
+		{
+			ApplicationLifetime.StopApplication();
+			base.OnStop();
+		}
+	}
+}
---
+++ netcore/windows-service/demo/WinService/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WinService.HostedServices.FileWriter;
+using WinService.Infrastructure;
+
+namespace WinService
+{
+	internal class Program
+	{
+		private static async Task Main(string[] args)
+		{
+			bool isService = !(Debugger.IsAttached || args.ToList().Contains("--console"));
+
+			IHostBuilder builder = new HostBuilder()
+				.ConfigureAppConfiguration(o => o.AddIniFile("appsettings.ini", false))
+				.ConfigureServices((hostContext, services) =>
+				{
+					services.AddHostedService<FileWriterService>();
+				});
+
+			if (isService)
+			{
+				await builder.RunAsServiceAsync();
+			}
+			else
+			{
+				await builder.RunConsoleAsync();
+			}
+		}
+	}
+}
